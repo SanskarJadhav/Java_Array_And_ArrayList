cf9734e7fdb390e16315dc31b595b3a87ff2997c
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-02-28 20:02:11 +0530

    Update NearestNumbers.java

--- NearestNumbers.java
+++ NearestNumbers.java
@@ -1,6 +1,7 @@
 public class NearestNumbers {
     static int[] numbers;
     public static int findNearestNumbers(){
+        // creating object for UserInput class
         UserInput input = new UserInput();
         numbers = input.createArrayUsingBufferedReader();
         // we will initialise minindex as 0
@@ -11,11 +12,13 @@ public class NearestNumbers {
         // then run a for loop to check the rest of the pairs of numbers
         for (int i = 1; i < numbers.length-1; i++){
             int dif = Math.abs(numbers[i+1] - numbers[i]);
+            // updating minvalue and minindex whenever smaller difference is found
             if (dif < minvalue){
                 minvalue = dif;
                 minindex = i;
             }
         }
+        // returning the index of the first number in the nearest numbers pair
         return minindex;
     }
 }
