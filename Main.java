e667a0d96985a29519cc8bc584a7fcbc2afedc21
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-02-28 21:09:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
 		EvenOrOdd eo = new EvenOrOdd();
 		NearestNumbers nn = new NearestNumbers();
 		ArrayAndArrayList aal = new ArrayAndArrayList();
-                int run;
+		int run;
 	    	// do while loop: run again until a condition is no longer satisfied
 		do{
 			Scanner sc = new Scanner(System.in);
