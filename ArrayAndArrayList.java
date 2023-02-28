9ad82a0d5ee9146439fb1906ac5f20ff878968cc
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-02-28 19:57:22 +0530

    Update ArrayAndArrayList.java

--- ArrayAndArrayList.java
+++ ArrayAndArrayList.java
@@ -3,11 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ArrayAndArrayList {
+    // defining an arraylist consisting of Integer type data
     ArrayList<Integer> namesArrayList = new ArrayList<Integer>();
     void arraytoArrayList()
     {
+        // creating object for UserInput class
         UserInput input = new UserInput();
         int[] numbers = input.createArrayUsingBufferedReader();
+        // advanced for loop to add each entered number into the arraylist
         for (int i : numbers) namesArrayList.add(i);
         System.out.println("Converting to ArrayList: ");
         System.out.println(namesArrayList);
@@ -17,11 +20,13 @@ public class ArrayAndArrayList {
     {
         // converting arraylist back to array
         int arraynum[] = new int[namesArrayList.size()];
+        // adding numbers from arraylist to an array
         for(int i=0; i<namesArrayList.size(); i++)
         {
             arraynum[i]=namesArrayList.get(i);
         }
         System.out.println("Converting back to Array: ");
+        // advanced for loop to display array
         for(int n : arraynum) System.out.print(n + " ");
         System.out.println();
     }
