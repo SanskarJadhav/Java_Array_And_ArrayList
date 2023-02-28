7d4fe2bd58609fbcb52fb3459692ed6a1853bc24
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-02-28 20:00:27 +0530

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -6,29 +6,32 @@ public class EvenOrOdd {
         System.out.println();
     }
     public static void splitEvenAndOddArrays(){
+        // creating object for UserInput class
         UserInput input = new UserInput();
         int evenSize = 0;
         int oddSize = 0;
         numbers = input.createArrayUsingBufferedReader();
+        // for loop to calculate number of even and number of odd values in entered array
         for (int number : numbers) {
             if (number % 2 == 0)
                 evenSize++;
             else
                 oddSize++;
         }
-        // odd and even arrays with size
+        // defining even and odd arrays with calculated sizes
         int[] even = new int[evenSize];
         int[] odd = new int[oddSize];
-        // odd and even array iterator
+        // even and odd array iterator
         int j = 0;
         int k = 0;
+        // separating and adding each number to respective array
         for (int number : numbers) {
             if (number % 2 == 0)
                 even[j++] = number;
             else
                 odd[k++] = number;
         }
-        // print array method
+        // calling print array method in same class
         System.out.print("Even Array: ");
         printArray(even);
         System.out.print("Odd Array: ");
